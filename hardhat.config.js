68441718c48ce676482b8ac68dc6dc237c237291
MASDXI <sirawit42t@example.com>
2023-11-09 17:00:29 +0700

    enable optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,15 @@ require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.14",
+  solidity: {
+    version: "0.8.14",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 450
+      }
+    }
+  },
   gasReporter: {
     enabled: true
   }
